5df7d75d960d3eca91200431f2f153c6ed97a06c
Sarah Beate Sandnes <sarahbeate@gmail.com>
2014-01-09 01:33:35 +0100

    SVEIP-0 Initiated a test

---
+++ test/no/sarah/sveiper/BrettfeltTest.java
@@ -0,0 +1,15 @@
+package no.sarah.sveiper;
+
+import org.junit.Before;
+
+/**
+ * Tests for {@link Brettfelt}
+ *
+ * @author Sarah Beate Sandnes
+ */
+public class BrettfeltTest {
+
+    @Before
+    public void setUp() {
+    }
+}

a2646c22f68bcdc2e8f7380a9bf57416076ff83f
Sarah Beate Sandnes <sarahbeate@gmail.com>
2014-01-09 01:32:17 +0100

    SVEIP-0 Litt brettfelt-refaktor.

--- src/no/sarah/sveiper/Brettfelt.java
+++ src/no/sarah/sveiper/Brettfelt.java
@@ -1,175 +1,207 @@
 package no.sarah.sveiper;
 
-import java.util.Timer;
-
 import android.content.Context;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-//@SuppressLint("ViewConstructor")
 public class Brettfelt extends Button {
-	private boolean marked;
-	private GestureDetector gestureDetector;
-	private boolean doubleClick;
-	private Brettfelt up;
-	private Brettfelt down;
-	private Brettfelt left;
-	private Brettfelt right;
-	private boolean clicked;
-	private TextView minesLeft;
-	private int[] coordinates;
-	final int numberOfFields;
+	private final GestureDetector gestureDetector;
+    private final int[] coordinates;
+    private Brettfelt up;
+    private Brettfelt down;
+    private Brettfelt left;
+    private Brettfelt right;
+    private TextView minesLeft;
+    private boolean currentClickIsDoubleClick;
+    private boolean isClicked;
+    private boolean isMarked;
+    final int numberOfFields;
 	final int numberOfMines;
-	final int hoyde;
-	final int bredde;
 
+    private int value;
 
-	//-1 = bomb, 0 = blank, 0< = number of bombs bordering
-	private int value;
-
-	public Brettfelt(Context context, int[] coordinates, int numberOfMines, int hoyde, int bredde) {
+	public Brettfelt(final Context context,
+                     final int[] coordinates,
+                     final int numberOfMines,
+                     final int height,
+                     final int width) {
 		super(context);
-		this.value = 9; //can never be 9 - this is for determening first click.
+		this.value = 9; //instansiated to 9, won't receive actual value until first field has been clicked.
 		this.setBackgroundResource(R.drawable.unclicked);
-		this.clicked = false;
+		this.isClicked = false;
 		this.numberOfMines = numberOfMines;
-		this.numberOfFields = hoyde*bredde;
+		this.numberOfFields = height * width;
 		this.coordinates = new int[2];
 		this.coordinates[0] = coordinates[0];
 		this.coordinates[1] = coordinates[1];
 
-		this.hoyde = hoyde;
-		this.bredde = bredde;
-		this.marked = false;
+        this.isMarked = false;
 		//dobbelklikk:
 	    gestureDetector = new GestureDetector(context, new GestureListener());
-	    //hold inne:
+	    //hold inne: ?????
 	    this.setOnTouchListener(new MyOnTouchEvent());
 	}
-	public void setMinesLeft(TextView minesLeft) {
+
+    public void setValue(final int value) {
+        this.value = value;
+    }
+	public void setMinesLeft(final TextView minesLeft) {
 		this.minesLeft = minesLeft;
 	}
-	//set-metoder for pekere
-	public void setUp(Brettfelt up) {
+
+    //set-metoder for nabopekere:
+	public void setUp(final Brettfelt up) {
 		this.up = up;
 	}
-	public void setDown(Brettfelt down) {
+    public void setDown(final Brettfelt down) {
 		this.down = down;
 	}
-	public void setRight(Brettfelt right) {
+    public void setRight(final Brettfelt right) {
 		this.right = right;
 	}
-	public void setLeft(Brettfelt left) {
+    public void setLeft(final Brettfelt left) {
 		this.left = left;
 	}
-	public void setCoordinates(int[] coordinates) {
-		this.coordinates = coordinates;
-	}
-	public void setValue(int value) {
-		this.value = value;
-	}
-	//sjekk om har peker til:
+
+	//nabopeker-sjekkere:
     boolean hasUp() {
-    	return (this.up != null);
+    	return (up != null);
     }
     boolean hasDown() {
-    	return (this.down != null);
+    	return (down != null);
     }
     boolean hasRight() {
-    	return (this.right != null);
+    	return (right != null);
     }
     boolean hasLeft() {
-    	return (this.left != null);
+    	return (left != null);
     }
     boolean hasUpperRight() {
-    	return (this.up != null && this.right != null);
+    	return (up != null && right != null);
     }
     boolean hasUpperLeft() {
-    	return (this.up != null && this.left != null);
+    	return (up != null && left != null);
     }
     boolean hasLowerRight() {
-    	return (this.down != null && this.right != null);
+        return (down != null && right != null);
     }
     boolean hasLowerLeft() {
-    	return (this.down != null && this.left != null);
+        return (down != null && left != null);
     }
-    void respondToClick() {
-    	if (!this.clicked) {
-    		//if field is marked and then clicked, mark needs to be removed and subtracted from markedcount:
-    		this.clicked = true;
-    		if (this.marked) {
-    			this.marked = false;
-    			addToMinesLeft();
-    		}
 
-    		if (this.value==-1) { //bomb
-        		((LagBrett) getContext()).numberOfClickedFields++;
-        		this.setBackgroundResource(R.drawable.clicked);
-    			this.setBackgroundResource(R.drawable.bombe);
-    			this.setText("");
-    			((LagBrett) getContext()).gameOver();
-    			return;
-
-    		} else if (this.value>0 && this.value<9) { //number bordering
-        		((LagBrett) getContext()).IncreaseClicked();
-        		this.setBackgroundResource(R.drawable.clicked);
-    			this.setTextColor(Color.parseColor("#FFFFD5F7"));
-    			this.setText(""+value);
-
-    		} else if (this.value==0) { //empty field
-        		((LagBrett) getContext()).IncreaseClicked();
-        		this.setBackgroundResource(R.drawable.clicked);
-    			traverseZeroClick(this);
-
-    		} else if (this.value==9) { //first click
-    			//sett verdier paa alle osv:
-    			((LagBrett) getContext()).setValueOfFieldsExceptOnFirstClicked(this.coordinates);
-    			//naa kan vi klikke:
-    			this.clicked=false;
-    			this.marked=false;
-    			this.respondToClick();
-    		}
-    	}
-    	checkIfWon();
-    }
-    int findNumberOfMarkedAround() {
-    	int numberOfMarkedAround = 0;
-    	if (this.hasRight() && this.right.marked) {numberOfMarkedAround++;}
-    	if (this.hasUp() && this.up.marked) {numberOfMarkedAround++;}
-    	if (this.hasDown() && this.down.marked) {numberOfMarkedAround++;}
-    	if (this.hasLeft() && this.left.marked) {numberOfMarkedAround++;}
-    	if (this.hasUpperRight() && this.up.right.marked) {numberOfMarkedAround++;}
-    	if (this.hasLowerRight() && this.down.right.marked) {numberOfMarkedAround++;}
-    	if (this.hasLowerLeft() && this.down.left.marked) {numberOfMarkedAround++;}
-    	if (this.hasUpperLeft() && this.up.left.marked) {numberOfMarkedAround++;}
-    	return numberOfMarkedAround;
-    }
-    void clickAllSurroundingUnclickedUnmarked() {
-    	if (this.hasRight() && !this.right.clicked && !this.right.marked) {this.right.respondToClick();}
-    	if (this.hasUp() && !this.up.clicked && !this.up.marked) {this.up.respondToClick();}
-    	if (this.hasDown() && !this.down.clicked && !this.down.marked) {this.down.respondToClick();}
-    	if (this.hasLeft() && !this.left.clicked && !this.left.marked) {this.left.respondToClick();}
-    	if (this.hasUpperRight() && !this.up.right.clicked && !this.up.right.marked) {this.up.right.respondToClick();}
-    	if (this.hasLowerRight() && !this.down.right.clicked && !this.down.right.marked) {this.down.right.respondToClick();}
-    	if (this.hasLowerLeft() && !this.down.left.clicked && !this.down.left.marked) {this.down.left.respondToClick();}
-    	if (this.hasUpperLeft() && !this.up.left.clicked && !this.up.left.marked) {this.up.left.respondToClick();}
-    }
+    /**
+     * Reveals bomb on field.
+     */
     public void displayBombIfBomb() {
-    	if (this.value == -1) {
-			this.setBackgroundResource(R.drawable.bombe);
-    	}
+        if (value == -1) {
+            setBackgroundResource(R.drawable.bombe);
+        }
     }
-    void checkIfWon() {
-    	if (((LagBrett) getContext()).numberOfClickedFields == numberOfFields-numberOfMines) {
-			((LagBrett) getContext()).wonTheGame();
-    	}
+
+    private void respondToClick() {
+        if (isClicked) return;
+
+        isClicked = true;
+
+        removeMark();
+
+        if (value==-1) {
+            respondToBombClick();
+            return;
+
+        } else if (value>0 && value<9) {
+            respondToNumberedFieldClick();
+
+        } else if (value==0) {
+            respondToEmptyFieldClick();
+
+        } else if (value==9) {
+            respondToFirstClick();
+
+        } else {
+            throw new IllegalArgumentException("Field value cannot be <0 or >9");
+        }
+
+        if (allBombFreeFieldsHaveBeenRevealed()) {
+            finishGame();
+        }
     }
-    void traverseZeroClick(Brettfelt felt) {
+
+    private void finishGame() {
+        ((LagBrett) getContext()).wonTheGame();
+    }
+
+    private void respondToFirstClick() {
+        //sett verdier paa alle osv:
+        ((LagBrett) getContext()).setValueOfFieldsExceptOnFirstClicked(this.coordinates);
+        //naa kan vi klikke:
+        this.isClicked =false;
+        this.isMarked =false;
+        this.respondToClick();
+    }
+
+    private void respondToEmptyFieldClick() {
+        ((LagBrett) getContext()).IncreaseClicked();
+        this.setBackgroundResource(R.drawable.clicked);
+        traverseEmptyFieldClick(this);
+    }
+
+    private void respondToNumberedFieldClick() {
+        ((LagBrett) getContext()).IncreaseClicked();
+        this.setBackgroundResource(R.drawable.clicked);
+        this.setTextColor(Color.parseColor("#FFFFD5F7"));
+        this.setText(""+value);
+    }
+
+    private void respondToBombClick() {
+        ((LagBrett) getContext()).numberOfClickedFields++;
+        this.setBackgroundResource(R.drawable.clicked);
+        this.setBackgroundResource(R.drawable.bombe);
+        this.setText("");
+        ((LagBrett) getContext()).gameOver();
+    }
+
+    private void removeMark() {
+        if (this.isMarked) {
+            this.isMarked = false;
+            addToMinesLeft();
+        }
+    }
+
+    private void clickAllSurroundingUnclickedUnmarked() {
+    	if (hasRight() && !this.right.isClicked && !this.right.isMarked) {this.right.respondToClick();}
+    	if (hasUp() && !this.up.isClicked && !this.up.isMarked) {this.up.respondToClick();}
+    	if (hasDown() && !this.down.isClicked && !this.down.isMarked) {this.down.respondToClick();}
+    	if (hasLeft() && !this.left.isClicked && !this.left.isMarked) {this.left.respondToClick();}
+    	if (hasUpperRight() && !this.up.right.isClicked && !this.up.right.isMarked) {this.up.right.respondToClick();}
+    	if (hasLowerRight() && !this.down.right.isClicked && !this.down.right.isMarked) {this.down.right.respondToClick();}
+    	if (hasLowerLeft() && !this.down.left.isClicked && !this.down.left.isMarked) {this.down.left.respondToClick();}
+    	if (hasUpperLeft() && !this.up.left.isClicked && !this.up.left.isMarked) {this.up.left.respondToClick();}
+    }
+
+    private int findNumberOfMarkedAround() {
+        int numberOfMarkedAround = 0;
+        if (hasRight() && this.right.isMarked) {numberOfMarkedAround++;}
+        if (hasUp() && this.up.isMarked) {numberOfMarkedAround++;}
+        if (hasDown() && this.down.isMarked) {numberOfMarkedAround++;}
+        if (hasLeft() && this.left.isMarked) {numberOfMarkedAround++;}
+        if (hasUpperRight() && this.up.right.isMarked) {numberOfMarkedAround++;}
+        if (hasLowerRight() && this.down.right.isMarked) {numberOfMarkedAround++;}
+        if (hasLowerLeft() && this.down.left.isMarked) {numberOfMarkedAround++;}
+        if (hasUpperLeft() && this.up.left.isMarked) {numberOfMarkedAround++;}
+        return numberOfMarkedAround;
+    }
+
+    /**
+     * Called if an empty field is clicked, and opens all reachable empty fields and their bordering number-fields.
+     *
+     * @param felt An empty field (value == 0).
+     */
+    private void traverseEmptyFieldClick(final Brettfelt felt) {
     	if (felt.hasUp()) {
     		this.up.respondToClick();
     	}
@@ -194,23 +226,26 @@ public class Brettfelt extends Button {
     	if (felt.hasLowerLeft()) {
     		this.down.left.respondToClick();
     	}
-
     }
-	@Override
-	public boolean onTouchEvent(MotionEvent e) {
-		boolean hopp = gestureDetector.onTouchEvent(e);
 
-		if (doubleClick && findNumberOfMarkedAround()==this.value && clicked) {
-			Log.d("mmm", "og hit samtidig");
+    private boolean allBombFreeFieldsHaveBeenRevealed() {
+        return ((LagBrett) getContext()).numberOfClickedFields == numberOfFields-numberOfMines;
+    }
+
+    @Override
+	public boolean onTouchEvent(final MotionEvent e) {
+		final boolean hopp = gestureDetector.onTouchEvent(e);
+
+		if (currentClickIsDoubleClick && findNumberOfMarkedAround() == value && isClicked) {
     		clickAllSurroundingUnclickedUnmarked();
-    	} else if (!clicked) {
-			if (marked) {
+    	} else if (!isClicked) {
+			if (isMarked) {
 				this.setBackgroundResource(R.drawable.marked);
 			} else {
 				this.setBackgroundResource(R.drawable.unclicked);
 			}
 
-			if (doubleClick) {
+			if (currentClickIsDoubleClick) {
 				respondToClick();
 			}
     	}
@@ -220,32 +255,31 @@ public class Brettfelt extends Button {
 	private class GestureListener extends GestureDetector.SimpleOnGestureListener {
 	    // event when double tap occurs
 	    @Override
-	    public boolean onDoubleTap(MotionEvent e) {
+	    public boolean onDoubleTap(final MotionEvent e) {
 	        return true;
 	    }
 
-	    //toggle/t-hack because this is somehow always called several times on doubleClick:
+	    //toggle-hack because this is somehow always called several times on doubleClick:
     	long test = System.currentTimeMillis();
-	    public boolean onDoubleTapEvent(MotionEvent e) {
-	    	long test2 = System.currentTimeMillis()-test;
+	    public boolean onDoubleTapEvent(final MotionEvent e) {
+	    	final long test2 = System.currentTimeMillis()-test;
 	    	if (test2 < 100) {
-	    		doubleClick = false;
+	    		currentClickIsDoubleClick = false;
 	    	} else {
-	    		doubleClick = true;
+	    		currentClickIsDoubleClick = true;
 	    		test = System.currentTimeMillis();
 	    	}
-	    	Log.d("mmm", ""+test2);
 	        return true;
 	    }
 
 	    @Override
-	    public boolean onDown(MotionEvent e) {
-	    	if (!clicked) {
-	    		if (marked) {
-	    			marked = false;
+	    public boolean onDown(final MotionEvent e) {
+	    	if (!isClicked) {
+	    		if (isMarked) {
+	    			isMarked = false;
 	    			addToMinesLeft();
 	    		} else {
-	    			marked = true;
+	    			isMarked = true;
 	    			subtractFromMinesLeft();
 	    		}
 	    	}
@@ -255,36 +289,35 @@ public class Brettfelt extends Button {
 
 	void addToMinesLeft() {
 		//change minesLeft aka. number of marked mines:
-		String minesLeftInfo = minesLeft.getText().toString();
-		int minesLeftInt = getMinesLeftFromString(minesLeftInfo);
-		String minesLeftText = getTextOnlyFromMinesLeftString(minesLeftInfo);
+		final String minesLeftInfo = minesLeft.getText().toString();
+		final int minesLeftInt = getMinesLeftFromString(minesLeftInfo);
+		final String minesLeftText = getTextOnlyFromMinesLeftString(minesLeftInfo);
 		minesLeft.setText(minesLeftText + (minesLeftInt+1));
 	}
 
 	void subtractFromMinesLeft() {
 		//change minesLeft aka. number of marked mines:
-		String minesLeftInfo = minesLeft.getText().toString();
-		int minesLeftInt = getMinesLeftFromString(minesLeftInfo);
-		String minesLeftText = getTextOnlyFromMinesLeftString(minesLeftInfo);
+		final String minesLeftInfo = minesLeft.getText().toString();
+		final int minesLeftInt = getMinesLeftFromString(minesLeftInfo);
+		final String minesLeftText = getTextOnlyFromMinesLeftString(minesLeftInfo);
 		minesLeft.setText(minesLeftText + (minesLeftInt-1));
 	}
 
-	int getMinesLeftFromString(String minesLeftInfo) {
+	private int getMinesLeftFromString(final String minesLeftInfo) {
 		return Integer.parseInt(minesLeftInfo.replaceAll("^-?[^\\d-]+",""));
 	}
 
-	String getTextOnlyFromMinesLeftString(String minesLeftInfo) {
+	private String getTextOnlyFromMinesLeftString(final String minesLeftInfo) {
 		return minesLeftInfo.replaceAll("-?\\d*$", "");
 	}
 
 	private class MyOnTouchEvent implements OnTouchListener {
-
 		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-			if(event.getAction() == MotionEvent.ACTION_DOWN && !Brettfelt.this.clicked){
+		public boolean onTouch(final View v, final MotionEvent event) {
+			if(event.getAction() == MotionEvent.ACTION_DOWN && !Brettfelt.this.isClicked){
 				Brettfelt.this.setBackgroundResource(R.drawable.pressed);
 			}
-			if(event.getAction() == MotionEvent.ACTION_UP && !Brettfelt.this.clicked){
+			if(event.getAction() == MotionEvent.ACTION_UP && !Brettfelt.this.isClicked){
 				Brettfelt.this.setBackgroundResource(R.drawable.unclicked);
 			}
 
